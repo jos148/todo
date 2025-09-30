79e39c5d2cfb95599d1045c69fcb3673826dc6a8
jos148 <ayojoshua123@gmail.com>
2025-09-30 12:03:25 +0100

    lay

--- src/app/Todo/components/todoform.tsx
+++ src/app/Todo/components/todoform.tsx
@@ -117,20 +117,6 @@ export default function TodoForm() {
       <Button type="submit" disabled={loading}>
         {loading ? "Adding..." : "Add"}
       </Button>
-      <Button
-        variant="outline"
-        onClick={() =>
-          toast("Event has been created", {
-            description: "Sunday, December 03, 2023 at 9:00 AM",
-            action: {
-              label: "Undo",
-              onClick: () => console.log("Undo"),
-            },
-          })
-        }
-      >
-        Show Toast
-      </Button>
     </form>
   );
 }
