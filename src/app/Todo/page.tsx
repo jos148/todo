5c319e8ef2be33a47445728c8eac7dae842b3f36
jos148 <ayojoshua123@gmail.com>
2025-10-04 21:16:14 +0100

    susp

--- src/app/Todo/page.tsx
+++ src/app/Todo/page.tsx
@@ -9,15 +9,13 @@ export default function Home() {
   return (
     <div className="bg-white dark:bg-muted w-full">
       <div className="border-b-2 border-gray-300">
-        
-<Suspense fallback={<div>Loading...</div>}>
-      <AppNav />
-      </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <AppNav />
+        </Suspense>
       </div>
-      
+
       <div className="flex h-screen mt-7 mx-10 mb-10">
         <Main />
-        
       </div>
     </div>
   );
